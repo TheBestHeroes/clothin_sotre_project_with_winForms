61de8b8761f0ca07f5c82aefca5be574f4871887
Arata shido <motahermomo0687@gmail.com>
2026-01-04 17:30:01 +0300

    Add project files.

---
+++ clothin_sotre_project_with_winForms/Form1.cs
@@ -0,0 +1,40 @@
+using Oracle.ManagedDataAccess.Client;
+
+namespace clothin_sotre_project_with_winForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string user = "HW";
+            string pass = "123";
+            string host = "localhost";
+            string port = "1521";
+            string serviceName = "XE"; // „Â„ Ãœ«
+
+            string conString =
+                $"User Id={user};Password={pass};Data Source={host}:{port}/{serviceName};";
+
+            // 2. „Õ«Ê·… «·« ’«·
+            using (OracleConnection con = new OracleConnection(conString))
+            {
+                try
+                {
+                    con.Open();
+                    MessageBox.Show(" „ «·« ’«· »ﬁ«⁄œ… »Ì«‰«  √Ê—«ﬂ· »‰Ã«Õ!", "‰Ã«Õ", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                    // ≈€·«ﬁ «·« ’«·  ·ﬁ«∆Ì« ⁄‰œ «·Œ—ÊÃ „‰ «·‹ using
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Œÿ√ ›Ì «·« ’«·: " + ex.Message, "›‘·", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+    }
+}
